f3d30049363933db9f9cd774f2574eee38468173
TedWU <wulited@outlook.com>
2022-02-22 22:50:07 +0800

    o

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Kana></Kana>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Kana from './components/Kana.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Kana,
   }
 }
 </script>
---
+++ src/components/Kana.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="(eachrows,index) in kana[0].items" :key="index" >
+      <div v-for="item in eachrows" :key="item.id" >
+        {{item.hiragana}} -- {{item.katakana}} -- {{item.romaji}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import kana from '../assets/kana.json';
+export default {
+  data() {
+    return {
+      kana
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
