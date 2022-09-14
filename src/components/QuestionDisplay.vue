78ca2b318dd0735e7d3cf8ae2e9acb9ddcccc6a7
TedWU <wulited@outlook.com>
2022-09-15 00:39:49 +0900

    add audio function

--- src/components/QuestionDisplay.vue
+++ src/components/QuestionDisplay.vue
@@ -1,49 +1,77 @@
 <template>
-	<div id="showHiragana" class="text-center  w-40 h-40 overflow-hidden mx-auto shadow-box-inner  rounded-2xl text-red-400 ">
-		<div class="grid grid-cols-1 gap-y-5 justify-items-center my-4 text-9xl inner_text_shadow transform duration-200" 
-			:class="{' -translate-y-36 ':transToggle, } "
-		>
-			<div class="transition duration-200"> {{getKana(gyo,dan,kanaType)}} </div>
-			<div class="transition duration-500" v-show="transToggle"> {{getKana(gyo,dan,'romaji')}} </div>
-		</div>
-	</div>
+  <div
+    id="showHiragana"
+    class="
+      text-center
+      w-40
+      h-40
+      overflow-hidden
+      mx-auto
+      shadow-box-inner
+      rounded-2xl
+      text-red-400
+    "
+  >
+    <div
+      class="
+        grid grid-cols-1
+        gap-y-5
+        justify-items-center
+        my-4
+        text-9xl
+        inner_text_shadow
+        transform
+        duration-200
+      "
+      :class="{ ' -translate-y-36 ': transToggle }"
+    >
+      <div class="transition duration-200 cursor-pointer select-none">
+        {{ getKana(gyo, dan, kanaType) }}
+      </div>
+      <div class="transition duration-500 select-none" v-show="transToggle">
+        {{ getKana(gyo, dan, "romaji") }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import kana from '../assets/kana.json'
+import kana from "../assets/kana.json";
 export default {
-	props:{ //kanaType: id/hiragana/katakana/romaji
-		gyo: {type: Number, default: 0},
-		dan: {type: Number, default: 0},
-		kanaType: {type: String, default: 'hiragana'},
-		transToggle:{type: Boolean,default: false}
-	},
-
+  props: {
+    //kanaType: id/hiragana/katakana/romaji
+    gyo: { type: Number, default: 0 },
+    dan: { type: Number, default: 0 },
+    kanaType: { type: String, default: "hiragana" },
+    transToggle: { type: Boolean, default: false },
+  },
   data() {
     return {
-      kana
-			}
+      kana,
+			publicPath: process.env.BASE_URL,
+			audioPath: ''
+    };
   },
-	methods:{
-		getKana(nowGyo,nowDan,kanaType){
-			let items = this.kana[0].items
-			let kana = items[nowGyo][nowDan][kanaType]
-			if (kana){
-				return kana
-			}else{
-				return "X"
-			}
-			
-		},
-		
-	}
-}
+  methods: {
+    getKana(nowGyo, nowDan, kanaType) {
+      let items = this.kana[0].items
+      let kana = items[nowGyo][nowDan][kanaType]
+      let romaji = items[nowGyo][nowDan]['romaji']
+      if (kana) {
+				this.audioPath = 'audio/'+romaji+'.mp3'
+        return kana;
+
+      } else {
+        return "X";
+      }
+    },
+
+  },
+};
 </script>
 
 <style>
-.inner_text_shadow
-{
-    text-shadow: -3px -3px 8px rgb(227,212,212), 3px 3px 7px rgb(196,171,171);
+.inner_text_shadow {
+  text-shadow: -3px -3px 8px rgb(227, 212, 212), 3px 3px 7px rgb(196, 171, 171);
 }
-
 </style>
--- src/components/ScoreBar.vue
+++ src/components/ScoreBar.vue
@@ -1,49 +1,49 @@
 <template>
-<div class="shadow-md pb-2 mb-3 ">
-  <div class="grid grid-cols-8 gap-x-3 text-xl text-red-400">
-    <div id="sakuraScoreBar" class=" col-span-3  flex ">
-      <transition-group tag="p" name="sakuraDrop" class="md:mx-auto">
-        <span class="sakura" v-for="i in currentChance" :key="i">
-          &#127800;
-        </span>
-      </transition-group>
+  <div class="shadow-md pb-2 mb-3">
+    <div class="grid grid-cols-8 gap-x-3 text-xl text-red-400">
+      <div id="sakuraScoreBar" class="col-span-3 flex">
+        <transition-group tag="p" name="sakuraDrop" class="md:mx-auto">
+          <span class="sakura select-none" v-for="i in currentChance" :key="i">
+            &#127800;
+          </span>
+        </transition-group>
+      </div>
+      <a
+        href="https://github.com/herointene/kanakill"
+        id="appNameShow"
+        target="_blank"
+        class="col-span-2 font-bold inner_text_shadow"
+      >
+        @KanaKill
+      </a>
+      <div id="grid-6"></div>
+      <div
+        ontouchstart=""
+        @click="switchType"
+        class="rounded-xl cursor-pointer w-16 text-opacity-70 select-none"
+        :class="typeFrom === 'hiragana' ? 'shadow-box-2' : 'shadow-box-inner'"
+      >
+        {{ typeFrom.slice(0, 4) }}
+      </div>
     </div>
-    <a href="https://github.com/herointene/kanakill" 
-      id="appNameShow" target="_blank"
-      class=" col-span-2 font-bold inner_text_shadow "
-    >
-    @KanaKill
-    </a>
-    <div id="grid-6"></div>
-    <div ontouchstart="" @click="switchType"
-      class=" rounded-xl  cursor-pointer  w-16 text-opacity-70"
-      :class="typeFrom==='hiragana'? 'shadow-box-2':'shadow-box-inner'"
-    >
-      {{typeFrom.slice(0,4)}}
-    </div>
-
-    
-
   </div>
-</div>
 </template>
 
 <script>
 // import 'animate.css'
 export default {
-  props:['currentChance', 'typeFrom'],
+  props: ["currentChance", "typeFrom"],
   methods: {
-    switchType(){
-      if(this.typeFrom === 'hiragana'){
-        this.$emit('typeFrom','katakana') // send 'katakana' to TestBlock comp, 
+    switchType() {
+      if (this.typeFrom === "hiragana") {
+        this.$emit("typeFrom", "katakana"); // send 'katakana' to TestBlock comp,
         //the event emit will call a function.
-      }else{
-        this.$emit('typeFrom','hiragana')
+      } else {
+        this.$emit("typeFrom", "hiragana");
       }
-      
-    }
+    },
   },
-}
+};
 </script>
 
 <style>
@@ -58,12 +58,10 @@ export default {
 
 .sakuraDrop-leave-to {
   opacity: 0;
-  transform:  translateY(80px) rotate(360deg);
+  transform: translateY(80px) rotate(360deg);
 }
 
-
-.inner_text_shadow
-{
-    text-shadow: -3px -3px 8px #eeebeb, 3px 3px 7px #c4abab;
+.inner_text_shadow {
+  text-shadow: -3px -3px 8px #eeebeb, 3px 3px 7px #c4abab;
 }
 </style>
